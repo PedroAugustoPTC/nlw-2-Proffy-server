9396a61bfd9779f42d084722700cb2d15521c3c2
unknown <pams693@gmail.com>
2020-08-06 17:10:07 -0300

    filtro de classe

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import ClassesControler from './controllers/ClassesController';
+
+const routes = express.Router();
+const classesController = new ClassesControler();
+
+
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import routes from './routes';
 
 //Express é o framework Node mais popular e a biblioteca subjacente para uma série de outros frameworks do Node. O Express oferece soluções para:
 
@@ -6,13 +7,11 @@ import express from 'express';
 //Integrar "view engines" para inserir dados nos templates.
 //Definir as configurações comuns da aplicação web, como a porta a ser usada para conexão e a localização dos modelos que são usados para renderizar a resposta.
 //Adicionar novos processos de requisição por meio de "middleware" em qualquer ponto da "fila" de requisições.
-
 const app = express();
 
-app.use(express.json);
-
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello Wolrd'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333); // Espera a requisição HTTP do cliente atravé da porta 3333
+
+
